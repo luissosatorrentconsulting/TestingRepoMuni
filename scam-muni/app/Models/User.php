1f1cbd156a023875c45a2718c245e1dad439edad
Luis Sosa <luis.sosa@torrentconsulting.com>
2026-04-25 18:18:27 -0600

    cambio para componer dashboar

--- scam-muni/app/Models/User.php
+++ scam-muni/app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser; 
+use Filament\Panel; 
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -45,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; 
     }
 }
