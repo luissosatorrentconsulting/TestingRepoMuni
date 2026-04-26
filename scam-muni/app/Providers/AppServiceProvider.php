c3b7861956b9052627092ec0ca6a6e4c4dc9c70c
Luis Sosa <luis.sosa@torrentconsulting.com>
2026-04-25 18:31:51 -0600

    changfa appa

--- scam-muni/app/Providers/AppServiceProvider.php
+++ scam-muni/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,5 +20,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
 {
     \App\Models\Asignacion::observe(\App\Observers\AsignacionObserver::class);
+    if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
 }
 }
