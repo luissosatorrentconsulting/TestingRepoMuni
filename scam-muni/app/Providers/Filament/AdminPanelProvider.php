8b3b2f9cdc2431884d86ae122d43b0f91654fcd1
Luis Sosa <luis.sosa@torrentconsulting.com>
2026-09-17 23:32:53 -0600

    g

--- scam-muni/app/Providers/Filament/AdminPanelProvider.php
+++ scam-muni/app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('MuniSys')
             ->colors([
                 'primary' => Color::Amber,
             ])
